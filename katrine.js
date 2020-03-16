d6cb02e2829efa8bbbce60da116aff63944e78d2
Søren Eskildsen <ses@trifork.com>
2020-03-16 21:48:20 +0100

    Help files to katrine and leonia

--- katrine.js
+++ katrine.js
@@ -1,3 +1,17 @@
 function showKatrineLectureSolution(lecture) {
-    
+    switch (lecture) {
+        case 1:
+            var canvas = document.getElementById("myCanvas");
+            var ctx = canvas.getContext("2d");
+            ctx.beginPath();
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.rect(100,100,10,10);
+            ctx.rect(150,150,10,10);
+            ctx.stroke();
+            break;
+        
+        case 2:
+            break;
+            
+    }
 }
