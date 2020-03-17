bcadbc8c050c0a6d2ac5c0be82782833e7cd4485
Søren Eskildsen <ses@trifork.com>
2020-03-17 21:25:46 +0100

    Undervisning 2

--- cecilie.js
+++ cecilie.js
@@ -3,10 +3,30 @@ function showCecilieLectureSolution(lecture) {
         case 1:
             var canvas = document.getElementById("myCanvas");
             var ctx = canvas.getContext("2d");
+
             ctx.beginPath();
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.rect(100,100,10,10);
-            ctx.rect(150,150,10,10);
+            {
+                var mitArray = new Array();
+                for (i=0; i<200; i++) {
+                    mitArray[i] = [
+                        Math.floor(Math.random()*(canvas.width-5)),
+                        Math.floor(Math.random()*(canvas.height-5)),
+                        5,
+                        5
+                    ];
+                }
+
+                // Laver noget andet
+                for (i=0; i<mitArray.length; i++) {
+                    let x = mitArray[i][0];
+                    if ((mitArray[i][0] > 20)) {
+                        ctx.rect(mitArray[i][0], mitArray[i][1], mitArray[i][2], mitArray[i][3]);
+                    }
+                }
+
+            }
+            
             ctx.stroke();
             break;
         
