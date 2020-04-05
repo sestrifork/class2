d99f09da9ed5b7be5e405ce3d2a7184274a76848
Søren Eskildsen <ses@trifork.com>
2020-04-05 19:11:36 +0200

    Søren løsning
    Sørens løsning

--- soren.js
+++ soren.js
@@ -1,24 +1,22 @@
 function showSorenLectureSolution(lecture) {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    
+    // Clear canvas
+    ctx.beginPath();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "#7FFF00"; //dark green
+
     switch (lecture) {
         case 1:
-            var canvas = document.getElementById("myCanvas");
-            var ctx = canvas.getContext("2d");
-
-            
             for (i=0; i<10; i++) {
-                function drawrect() {
-                    ctx.beginPath();
-                    ctx.clearRect(0, 0, canvas.width, canvas.height);
-                    ctx.strokeRect(i*10, i*10, 10, 10);
-                    ctx.stroke();
-                }
-                setTimeout(drawrect,5000);
+                ctx.strokeRect(i*20, i*20, 10, 10);
             }
-            
             break;
         
         case 2:
             break;
             
     }
+    ctx.stroke();
 }
