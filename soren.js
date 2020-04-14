1d111f70299ccd94befaecd8ed0e7b5cfb5f9de9
Søren Eskildsen <ses@trifork.com>
2020-04-14 18:17:21 +0200

    Opgave 3 og lekter til lektion 4
    Opgave 3 og lekter til lektion 4

--- soren.js
+++ soren.js
@@ -7,6 +7,15 @@ class Person {
     }
 }
 
+function getColorRedBlackGreenRandom() {
+    let random_number = Math.floor(Math.random()*3);
+    switch (random_number) {
+        case 0: return "#FF0000";
+        case 1: return "#000000";
+        default: return "00FF00";
+    }
+}
+
 function isOverlapping(rect1, rect2){
     return ((rect2.x > (rect1.x-rect2.width)) && (rect2.x < (rect1.x+rect1.width)) &&
     (rect2.y > (rect1.y-rect2.height)) && (rect2.y < (rect1.y+rect1.height)));
@@ -87,38 +96,37 @@ function showSorenLectureSolution(lecture) {
             ctx.stroke();       
             break;
 
-            case 3:
-                var canvas = document.getElementById("myCanvas2");
-                var ctx = canvas.getContext("2d");
-                
-                // Clear canvas
-                ctx.beginPath();
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
+        case 3:
+            var canvas = document.getElementById("myCanvas2");
+            var ctx = canvas.getContext("2d");
             
-                var rects = [];
-                var boxsize = 50;
-                
-                // Først laver jeg et rektangel
-                for (i=0; i<1 ; i++) {
-                    rects.push(getRandomRect(canvas.width, canvas.height, boxsize));
-                }
-          
-                // så laver jeg et mere og tjekker om de overlapper
-                var rect2 = getRandomRect(canvas.width, canvas.height, boxsize);
-                if (isOverlapping(rects[0], rect2)) {
-                    console.log("De overlapper, så jeg smider det væk");
-                } else {
-                    rects.push(rect2);
-                }
-    
-                // the same as
-                for (i=0; i<rects.length; i++) {
-                    var rect = rects[i];
-                    ctx.strokeStyle = rect.strokeStyle;
-                    ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
-                }
-                ctx.stroke();       
-                break;            
-    }
+            // Clear canvas
+            ctx.beginPath();
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+            var rects = [];
+            var boxsize = 50;
+            
+            // Først laver jeg et rektangel
+            for (i=0; i<1 ; i++) {
+                rects.push(getRandomRect(canvas.width, canvas.height, boxsize));
+            }
+        
+            // så laver jeg et mere og tjekker om de overlapper
+            var rect2 = getRandomRect(canvas.width, canvas.height, boxsize);
+            if (isOverlapping(rects[0], rect2)) {
+                console.log("De overlapper, så jeg smider det væk");
+            } else {
+                rects.push(rect2);
+            }
 
+            // the same as
+            for (i=0; i<rects.length; i++) {
+                var rect = rects[i];
+                ctx.strokeStyle = rect.strokeStyle;
+                ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+            }
+            ctx.stroke();       
+            break;            
+    }
 }
