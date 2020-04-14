dfeaab85fac55fc94aa203165f39bc5383d3c34c
leoniafalck <62103391+leoniafalck@users.noreply.github.com>
2020-04-14 17:54:00 +0200

    Post lektion 2
    Rettelse til 100 stk

--- leonia.js
+++ leonia.js
@@ -1,3 +1,16 @@
+function isOverlapping(rect1, rect2){
+    return ((rect2.x > (rect1.x-rect2.width)) && (rect2.x < (rect1.x+rect1.width)) &&
+    (rect2.y > (rect1.y-rect2.height)) && (rect2.y < (rect1.y+rect1.height)));
+}
+function getRandomRect(width, height, boxsize){
+    return {
+        x:Math.floor(Math.random()*(width-boxsize)), 
+        y:Math.floor(Math.random()*(height-boxsize)), 
+        width:boxsize, 
+        height:boxsize, 
+        strokeStyle:"#7FFF00"
+    };
+}
 function showLeoniaLectureSolution(lecture) {
     switch (lecture) {
         case 1:
@@ -28,7 +41,44 @@ function showLeoniaLectureSolution(lecture) {
             }
         break ;
 
+        
         case 2:
+            var canvas = document.getElementById("myCanvas2");
+            var ctx = canvas.getContext("2d");
+            //clear canvas 
+            ctx.beginPath();
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            var rects = [];
+            var boxsize = 5;
+
+            //Der tilføjes (push()) random retangler
+            //for (i=0; i<1; i++) {
+                //rects.push(getRandomRect(canvas.width, canvas.height, boxsize));
+            //}
+            while (rects.length<100) {
+                //Retangel 2 tilføjes:
+                var rect2 = getRandomRect(canvas.width, canvas.height, boxsize);
+                var overlapping = false;
+                for (i=0; i<rects.length; i++) {
+                    if (isOverlapping(rects[i], rect2)) {
+                        overlapping = true;
+                        console.log("Overlapping"); 
+                    }
+                }
+                // Hvis retanglet ikke overlapper de andre, tilføjes de i arrayet. 
+                if (overlapping == false) { 
+                    rects.push(rect2);
+                }
+            }
+            //koden skrives ud - yes!
+            for (i=0; i<rects.length; i++) {
+                var rect = rects[i];
+                ctx.strokeStyle = "#0000FF";
+                ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+            }
+
+            ctx.stroke();       
 
         break;
     }
